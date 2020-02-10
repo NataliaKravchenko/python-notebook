13b0eee8280aefd517f21c0f29f4b2171689666d
Nataliia Kravchenko <kravchenko.nathalia@gmail.com>
2020-02-09 23:22:58 -0500

    The first commit

--- python3 testing_scripts.py
+++ python3 testing_scripts.py
@@ -14,6 +14,21 @@ print(mylist)
 print(f'{type(mynumber)}, {type(myfloat)}, {type(mystring)}, {type(myboolean)}, {type(mylist)}')
 
 #Python Script 2
-
 my_list=[1,2,3,4,5,6,7,8,9]
+selection=my_list[3:8]
+new_list=selection*2
+print(new_list)
 
+#Python Script 3
+print(my_list[0:][::2])
+print(my_list[1:][::2])
+
+#Python Script 4
+x = list(range(1,101))
+for i in x:
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    if i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
